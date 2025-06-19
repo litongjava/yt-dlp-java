2427531d62b92cc4d72328981a7f582a0a602ee8
Tong Li <litongjava001@gmail.com>
2025-06-19 13:47:26 -1000

    update YtDlpUtils

--- src/main/java/com/litongjava/yt/utils/YtDlpUtils.java
+++ src/main/java/com/litongjava/yt/utils/YtDlpUtils.java
@@ -2,11 +2,10 @@ package com.litongjava.yt.utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
+import com.litongjava.tio.utils.commandline.ProcessResult;
 import com.litongjava.yt.YtDlp;
 import com.litongjava.yt.builder.YtDlpOption;
 import com.litongjava.yt.builder.YtDlpOptionBuilder;
@@ -16,70 +15,140 @@ public class YtDlpUtils {
   public static final String URL_TEMPLATE = "https://www.youtube.com/watch?v=%s";
   public static final String DOWNLOAD_FOLDER = "download";
 
-  public static File downloadMp4(String videoId, boolean quiet) {
+  public static ProcessResult downloadMp4(String videoId, boolean quiet) throws IOException, InterruptedException {
+
+    String folderName = "mp4";
+    String format = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4";
+    String suffix = ".mp4";
+    return downloadVideo(videoId, quiet, format, folderName, suffix);
+  }
+
+  public static ProcessResult downloadMp3(String videoId, boolean quiet) throws IOException, InterruptedException {
+    String folderName = "/mp3";
+    String format = "mp3";
+    String suffix = ".mp3";
+    return downloadAudio(videoId, quiet, format, folderName, suffix);
+  }
+
+  private static ProcessResult downloadVideo(String videoId, boolean quiet, String format, String subFolderName, String suffix) throws IOException, InterruptedException {
+    String folder = DOWNLOAD_FOLDER + File.separator + videoId + File.separator + subFolderName;
+
+    File outDir = new File(folder);
+    File[] listFiles = outDir.listFiles();
+
+    if (listFiles != null && listFiles.length > 0) {
+      for (File file : listFiles) {
+        if (file.getName().endsWith(suffix)) {
+          return new ProcessResult(file, true);
+        }
+      }
+      
+    }
+
     String url = String.format(URL_TEMPLATE, videoId);
-    String folder = DOWNLOAD_FOLDER + "/" + videoId + "/mp4";
     String output = folder + "/%(title)s.%(ext)s";
 
-    YtDlpOptionBuilder builder = new YtDlpOptionBuilder().url(url)
-        // 优先 mp4 视频+音频，否则回退到 mp4
-        .format("bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4").output(output);
-
+    YtDlpOptionBuilder builder = new YtDlpOptionBuilder().url(url).audio().format(format).output(output);
     if (quiet) {
       builder.quiet();
     }
-
     YtDlpOption options = builder.build();
-    YtDlp.execute(options);
 
-    File[] files = new File(folder).listFiles();
-    return (files != null && files.length > 0) ? files[0] : null;
-  }
-
-  public static File downloadMp3(String videoId, boolean quiet) {
-    
-    String folder = DOWNLOAD_FOLDER + "/" + videoId + "/mp3";
-    File[] listFiles = new File(folder).listFiles();
+    ProcessResult result = YtDlp.execute(folder, options);
+    listFiles = outDir.listFiles();
     if (listFiles != null && listFiles.length > 0) {
-      if(listFiles[0].getName().endsWith(".mp3")) {
-        return listFiles[0];
+      for (File file : listFiles) {
+        if (file.getName().endsWith(suffix)) {
+          return new ProcessResult(file, true);
+        }
       }
     }
-    
+    return result;
+  }
+  
+  private static ProcessResult downloadAudio(String videoId, boolean quiet, String format, String subFolderName, String suffix) throws IOException, InterruptedException {
+    String folder = DOWNLOAD_FOLDER + File.separator + videoId + File.separator + subFolderName;
+
+    File outDir = new File(folder);
+    File[] listFiles = outDir.listFiles();
+
+    if (listFiles != null && listFiles.length > 0) {
+      for (File file : listFiles) {
+        if (file.getName().endsWith(suffix)) {
+          return new ProcessResult(file, true);
+        }
+      }
+      
+    }
+
     String url = String.format(URL_TEMPLATE, videoId);
     String output = folder + "/%(title)s.%(ext)s";
-    YtDlpOptionBuilder builder = new YtDlpOptionBuilder().url(url).audio().audioFormat("mp3").output(output);
+
+    YtDlpOptionBuilder builder = new YtDlpOptionBuilder().url(url).audio().audioFormat(format).output(output);
     if (quiet) {
       builder.quiet();
     }
     YtDlpOption options = builder.build();
 
-    YtDlp.execute(options);
-    listFiles = new File(folder).listFiles();
+    ProcessResult result = YtDlp.execute(folder, options);
+    listFiles = outDir.listFiles();
     if (listFiles != null && listFiles.length > 0) {
-      if(listFiles[0].getName().endsWith(".mp3")) {
-        return listFiles[0];
+      for (File file : listFiles) {
+        if (file.getName().endsWith(suffix)) {
+          return new ProcessResult(file, true);
+        }
       }
     }
-    return null;
+    return result;
   }
 
-  public static String downlodSubtitle(String videoId, boolean quiet) {
-    String url = String.format(URL_TEMPLATE, videoId);
+  public static ProcessResult downlodSubtitle(String videoId, boolean quiet) throws IOException, InterruptedException {
+
     String folder = DOWNLOAD_FOLDER + "/" + videoId + "/sub";
+    String suffix = ".vtt";
+
+    File outDir = new File(folder);
+    File[] listFiles = outDir.listFiles();
+
+    if (listFiles != null && listFiles.length > 0) {
+      for (File file : listFiles) {
+        if (file.getName().endsWith(suffix)) {
+          List<String> lines = Files.readAllLines(file.toPath());
+          String subtitle = String.join(System.lineSeparator(), lines);
+          return new ProcessResult(subtitle, true);
+        }
+      }
+
+    }
+
+    String url = String.format(URL_TEMPLATE, videoId);
+
     String output = folder + "/%(title)s.%(ext)s";
     YtDlpOptionBuilder builder = new YtDlpOptionBuilder().url(url).output(output).writeSub().skipDownload();
     if (quiet) {
       builder.quiet();
     }
     YtDlpOption options = builder.build();
-    return execute(videoId, folder, options);
+
+    ProcessResult execute = YtDlp.execute(folder, options);
+    listFiles = outDir.listFiles();
+    if (listFiles != null && listFiles.length > 0) {
+      for (File file : listFiles) {
+        if (file.getName().endsWith(suffix)) {
+          List<String> lines = Files.readAllLines(file.toPath());
+          String subtitle = String.join(System.lineSeparator(), lines);
+          return new ProcessResult(subtitle);
+        }
+      }
+
+    }
+    return execute;
+
   }
 
-  public static String downloadAutoSubtitle(String videoId, boolean quiet) {
+  public static ProcessResult downloadAutoSubtitle(String videoId, boolean quiet) throws IOException, InterruptedException {
 
-    String url = "https://www.youtube.com/watch?v=%s";
-    url = String.format(url, videoId);
+    String url = String.format(URL_TEMPLATE, videoId);
     String folder = DOWNLOAD_FOLDER + "/" + videoId + "/vtt";
     String output = folder + "/%(title)s.%(ext)s";
 
@@ -88,12 +157,11 @@ public class YtDlpUtils {
       builder.quiet();
     }
     YtDlpOption options = builder.build();
-    return execute(videoId, folder, options);
+    return YtDlp.execute(folder, options);
   }
 
-  public static String downloadAutoSubtitle(String videoId, String type, boolean quiet) {
-    String url = "https://www.youtube.com/watch?v=%s";
-    url = String.format(url, videoId);
+  public static ProcessResult downloadAutoSubtitle(String videoId, String type, boolean quiet) throws IOException, InterruptedException {
+    String url = String.format(URL_TEMPLATE, videoId);
     String folder = DOWNLOAD_FOLDER + "/" + videoId + "/" + type;
     String output = folder + "/%(title)s.%(ext)s";
 
@@ -102,23 +170,7 @@ public class YtDlpUtils {
       builder.quiet();
     }
     YtDlpOption options = builder.build();
-    return execute(videoId, folder, options);
+    return YtDlp.execute(folder, options);
   }
 
-  private static String execute(String id, String folder, YtDlpOption options) {
-    YtDlp.execute(options);
-    File[] listFiles = new File(folder).listFiles();
-    if (listFiles != null && listFiles.length > 0) {
-      File subtitleFile = listFiles[0];
-      try {
-        List<String> lines = Files.readAllLines(subtitleFile.toPath(), StandardCharsets.UTF_8);
-        subtitleFile.delete();
-        Files.delete(Paths.get(DOWNLOAD_FOLDER, id + ""));
-        return String.join(System.lineSeparator(), lines);
-      } catch (IOException e) {
-        throw new RuntimeException("Failed to read file", e);
-      }
-    }
-    return null;
-  }
 }
--- src/test/java/com/litongjava/yt/utils/YtDlpUtilsTest.java
+++ src/test/java/com/litongjava/yt/utils/YtDlpUtilsTest.java
@@ -1,29 +1,70 @@
 package com.litongjava.yt.utils;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
+
+import com.litongjava.tio.utils.commandline.ProcessResult;
+import com.litongjava.yt.YtDlp;
 
 public class YtDlpUtilsTest {
 
   @Test
   public void downlodSubtitle() {
-    String subTitle = YtDlpUtils.downlodSubtitle("AMCUqgu_cTM", true);
-    System.out.println(subTitle);
+    try {
+      ProcessResult result = YtDlpUtils.downlodSubtitle("AMCUqgu_cTM", true);
+      System.out.println(result.getOutput());
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 
   @Test
   public void downloadMp3() {
-    File file = YtDlpUtils.downloadMp3("AMCUqgu_cTM", true);
-    System.out.println(file.getName());
-    System.out.println(file.exists());
+    try {
+      ProcessResult downloadMp3 = YtDlpUtils.downloadMp3("AMCUqgu_cTM", true);
+      File file = downloadMp3.getFile();
+      System.out.println(file);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
   }
-  
+
   @Test
   public void downloadMp4() {
-    File file = YtDlpUtils.downloadMp4("AMCUqgu_cTM", true);
-    System.out.println(file.getName());
-    System.out.println(file.exists());
+    ProcessResult result;
+    try {
+      result = YtDlpUtils.downloadMp4("AMCUqgu_cTM", true);
+      if (result.getFile() != null) {
+        File file = result.getFile();
+        System.out.println(file.getName());
+        System.out.println(file.exists());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void listFormat() {
+    try {
+      ProcessResult result = YtDlp.getAvailableFormats("https://www.youtube.com/watch?v=PnHMAVXpKg8");
+      System.out.println(result.getStdOut());
+      System.out.println(result.getStdErr());
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
